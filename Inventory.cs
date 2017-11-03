6ae38bb7c585f5e60c079fbaba7d3e531a82ef49
deixman1 <deix@yandex.ru>
2017-11-03 19:33:19 +0700

    Update Inventory.cs

--- Inventory.cs
+++ Inventory.cs
@@ -1,66 +1,40 @@
-int FeAmount = 0;
-int CbAmount = 0;
-int NiAmount = 0;
-int MgAmount = 0;
-int AuAmount = 0;
-int AgAmount = 0;
-int PtAmount = 0;
-int SiAmount = 0;
-int UAmount = 0;
-int StoneAmount = 0;
-int IceAmount = 0;
-List<IMyTerminalBlock> list = new List<IMyTerminalBlock>();
-void Main()
-{
-GridTerminalSystem.GetBlocksOfType<IMyCargoContainer>(list);
-FeAmount = 0;
-CbAmount = 0;
-NiAmount = 0;
-MgAmount = 0;
-AuAmount = 0;
-AgAmount = 0;
-PtAmount = 0;
-SiAmount = 0;
-UAmount = 0;
-StoneAmount = 0;
-IceAmount = 0;
-for (int i = 0; i < list.Count; i++) 
-{ 
-IMyTerminalBlock CargoOwner = list[i]; 
-if (CargoOwner != null) 
-{ 
- 
-var crateItems = CargoOwner.GetInventory(0).GetItems(); 
+    List<IMyTerminalBlock> list;
+    Dictionary<string, int> CountItems;
+    StringBuilder ListToWrite;
+    IMyTextPanel panel;
+    public Program()
+    {
+        panel = GridTerminalSystem.GetBlockWithName("Amount") as IMyTextPanel;
+    }
+    
+    public void Main(string args)
+    {
+        ListToWrite = new StringBuilder();
+        list = new List<IMyTerminalBlock>();
+        CountItems = new Dictionary<string, int>();
+        GridTerminalSystem.GetBlocks(list);
+        for (int i = 0; i < list.Count; i++)
+        {
+            if(list[i].HasInventory)
+            {
+                for (int j = 0; j < list[i].InventoryCount; j++)
+                {
+                    List<IMyInventoryItem> crateItems = list[i].GetInventory(j).GetItems();
+                    for (int k = 0; k < crateItems.Count; k++)
+                    {
+                        if (CountItems.ContainsKey(crateItems[k].Content.SubtypeName))
+                            CountItems[crateItems[k].Content.SubtypeName] += (int)crateItems[k].Amount;
+                        else
+                            CountItems.Add(crateItems[k].Content.SubtypeName, (int)crateItems[k].Amount);
+                    }
+                }
+            }
+        }
+        foreach (string TypeItem in CountItems.Keys)
+            ListToWrite.Append("Кол-во "+ TypeItem + ": " + CountItems[TypeItem].ToString()+"\n");
+        panel.WritePublicText(ListToWrite.ToString());
 
-for (int j = crateItems.Count - 1; j >= 0; j--) 
-{ 
-if (crateItems[j].Content.SubtypeName == "Iron") 
-FeAmount += (int)crateItems[j].Amount; 
-else if (crateItems[j].Content.SubtypeName == "Cobalt") 
-CbAmount += (int)crateItems[j].Amount; 
-else if (crateItems[j].Content.SubtypeName == "Nickel") 
-NiAmount += (int)crateItems[j].Amount; 
-else if (crateItems[j].Content.SubtypeName == "Magnesium") 
-MgAmount += (int)crateItems[j].Amount; 
-else if (crateItems[j].Content.SubtypeName == "Gold") 
-AuAmount += (int)crateItems[j].Amount; 
-else if (crateItems[j].Content.SubtypeName == "Silver") 
-AgAmount += (int)crateItems[j].Amount; 
-else if (crateItems[j].Content.SubtypeName == "Platinum") 
-PtAmount += (int)crateItems[j].Amount; 
-else if (crateItems[j].Content.SubtypeName == "Silicon") 
-SiAmount += (int)crateItems[j].Amount; 
-else if (crateItems[j].Content.SubtypeName == "Uranium") 
-UAmount += (int)crateItems[j].Amount; 
-else if (crateItems[j].Content.SubtypeName == "Stone") 
-StoneAmount += (int)crateItems[j].Amount; 
-else if (crateItems[j].Content.SubtypeName == "Ice") 
-IceAmount += (int)crateItems[j].Amount; 
-} 
-}
-}
-//}
-IMyTextPanel panel = GridTerminalSystem.GetBlockWithName("Amount") as IMyTextPanel;
-panel.WritePublicText("В КОНТЕЙНЕРАХ\nКол-во Железа: "+FeAmount.ToString()+"\n"+"Кол-во Кобальта: "+CbAmount.ToString()+"\n"+"Кол-во Никеля: "+NiAmount.ToString()+"\n"+"Кол-во Мг: "+MgAmount.ToString()+"\n"+"Кол-во Сильвера: "+AuAmount.ToString()+"\n"+"Кол-во Золота: "+AgAmount.ToString()+"\n"+"Кол-во Платины: "+PtAmount.ToString()+"\n"+"Кол-во Кремня: "+SiAmount.ToString()+"\n"+"Кол-во Урана: "+UAmount.ToString()+"\n"+"Кол-во Камня: "+StoneAmount.ToString()+"\n"+"Кол-во Льда: "+IceAmount.ToString());
-
-}
+    }
+    public void Save()
+    {
+    }
