9ea29c9ee46032ec24dd9894940dc09aec5544b7
deixman1 <deix@yandex.ru>
2020-10-26 04:03:45 +0300

    update

--- Inventory.cs
+++ Inventory.cs
@@ -1,40 +1,87 @@
-List<IMyTerminalBlock> list;
-Dictionary<string, int> CountItems;
-StringBuilder ListToWrite;
-IMyTextPanel panel;
-public Program()
-{
-    panel = GridTerminalSystem.GetBlockWithName("Amount") as IMyTextPanel;
-}
+using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using VRageMath;
+using VRage.Game;
+using Sandbox.ModAPI.Interfaces;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.Game.EntityComponents;
+using VRage.Game.Components;
+using VRage.Collections;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game.ModAPI.Ingame;
+using SpaceEngineers.Game.ModAPI.Ingame;
 
-public void Main(string args)
+namespace space
 {
-    ListToWrite = new StringBuilder();
-    list = new List<IMyTerminalBlock>();
-    CountItems = new Dictionary<string, int>();
-    GridTerminalSystem.GetBlocks(list);
-    for (int i = 0; i < list.Count; i++)
+    public sealed class Program : MyGridProgram
     {
-        if(list[i].HasInventory)
+        List<IMyTerminalBlock> list;
+        Dictionary<string, int> CountItems;
+        StringBuilder ListToWrite;
+        IMyTextPanel panel;
+        public Program()
         {
-            for (int j = 0; j < list[i].InventoryCount; j++)
+            panel = GridTerminalSystem.GetBlockWithName("Amount") as IMyTextPanel;
+        }
+
+        public void Main(string args)
+        {
+            ListToWrite = new StringBuilder();
+            list = new List<IMyTerminalBlock>();
+            CountItems = new Dictionary<string, int>();
+            GridTerminalSystem.GetBlocks(list);
+            for (int i = 0; i < list.Count; i++)
             {
-                List<IMyInventoryItem> crateItems = list[i].GetInventory(j).GetItems();
-                for (int k = 0; k < crateItems.Count; k++)
+                if (list[i].HasInventory && list[i].InventoryCount > 0)
                 {
-                    if (CountItems.ContainsKey(crateItems[k].Content.SubtypeName))
-                        CountItems[crateItems[k].Content.SubtypeName] += (int)crateItems[k].Amount;
-                    else
-                        CountItems.Add(crateItems[k].Content.SubtypeName, (int)crateItems[k].Amount);
+                    for (int j = 0; j < list[i].InventoryCount; j++)
+                    {
+                        var crateItems = new List<MyInventoryItem>();
+                        list[i].GetInventory(j).GetItems(crateItems);
+                        for (int k = 0; k < crateItems.Count; k++)
+                        {
+                            string itemType = crateItems[k].Type.TypeId.Replace("MyObjectBuilder_", "");
+                            string itemSubType = crateItems[k].Type.SubtypeId;
+                            int itemAmount = (int)crateItems[k].Amount;
+                            string itemSubAndType = itemType + "/" + itemSubType;
+                            if (CountItems.ContainsKey(itemSubAndType))
+                                CountItems[itemSubAndType] += itemAmount;
+                            else
+                                CountItems.Add(itemSubAndType, itemAmount);
+                        }
+                    }
                 }
             }
+            CountItems = CountItems.OrderBy(obj => obj.Key).ToDictionary(obj => obj.Key, obj => obj.Value);
+            int maxLength = 0;
+            int count = 1;
+            foreach (string TypeItem in CountItems.Keys)
+            {
+                string str = TypeItem + ": " + CountItems[TypeItem].ToString() + " ";
+                if (maxLength < str.Length)
+                    maxLength = str.Length;
+            }
+            foreach (string TypeItem in CountItems.Keys)
+            {
+                string str = TypeItem + ": " + CountItems[TypeItem].ToString();
+                if (count % 3 == 0)
+                    str += "\n";
+                else
+                    while (str.Length < maxLength)
+                        str += " ";
+                count++;
+                ListToWrite.Append(str);
+            }
+            if (maxLength*2 > 68)
+                panel.FontSize = 0.680f;
+            panel.WriteText(ListToWrite.ToString());
+        }
+
+        public void Save()
+        {
         }
     }
-    foreach (string TypeItem in CountItems.Keys)
-        ListToWrite.Append(TypeItem + ": " + CountItems[TypeItem].ToString()+"\n");
-    panel.WritePublicText(ListToWrite.ToString());
-}
-
-public void Save()
-{
+    
 }
